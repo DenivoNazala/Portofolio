5d168ff744a1d4dc847d6fda7baf475b9cfe0d5c
denivonazzala <denivoncandrania@gmail.com>
2025-04-15 11:29:08 +0700

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+
+import styles from "./Navbar.module.css"
+import {getImageUrl} from "../../utils";
+
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+        <a className={styles.tittle} href="/">Portofolio</a>
+        <div className={styles.menu}>
+            <img className={styles.menuBtn} src={getImageUrl("nav/menu-icon.png")}/>
+            <ul className={styles.menuItems}>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
